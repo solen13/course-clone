dadcfcbaec087b4b9ce0088bcd9709dd7f2c1a31
Uğur Şölen <76258170+solen13@users.noreply.github.com>
2022-10-29 11:11:36 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '../static/Foundation' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/Foundation.png' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css

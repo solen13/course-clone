12292aa1e40644ff3ca46fead1d70971437f6024
Uğur Şölen <76258170+solen13@users.noreply.github.com>
2022-10-29 10:55:42 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'clone-projecek',
+    title: 'Islam for Beginners',
     htmlAttrs: {
       lang: 'tr',
     },
@@ -11,7 +11,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '../static/Foundation' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
